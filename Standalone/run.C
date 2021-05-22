72db58dd7b793ef51cae46b7e2834593f07147a0
Indranil Das <indranil.das@cern.ch>
2021-05-22 20:06:28 +0530

    run file adding # skipping option

--- Standalone/run.C
+++ Standalone/run.C
@@ -1,6 +1,5 @@
 int run(const char* opt = "ptmin=30.0|ptmax=50.0|chi2cut=20.0|mtop=172.6|sample=TTbarPowheg|year=2016") // sample = data, bkg, signal
-{
-  
+{ 
   
   TString options(opt);
   TChain *chain = new TChain("RecoNtuple_Skim");
@@ -8,6 +7,9 @@ int run(const char* opt = "ptmin=30.0|ptmax=50.0|chi2cut=20.0|mtop=172.6|sample=
   string s;
   while(getline(fin,s)){
     //cout << s << endl;
+    // The following skips all lines of input file files.txt that starts with a '#'
+    if (s.rfind("#", 0) == 0) continue;
+
     chain->Add(s.c_str());
   }
   
