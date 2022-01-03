c71db719b04e0b75181ea574300ed9be0939d1d5
Indranil Das <indranil.das@cern.ch>
2022-01-03 07:57:55 +0530

    Apply PUID==0 following RBI

--- CBA_Skim/condor/createJdlFiles_cbaskim_syst.py
+++ CBA_Skim/condor/createJdlFiles_cbaskim_syst.py
@@ -72,7 +72,7 @@ for year in [2016,2017,2018]:
     jdlFile.write('Executable =  runCBASkim.sh \n')
     jdlFile.write(common_command)
     #condorOutDir1="/eos/user/i/imirza/idas/Output/cms-hcs-run2/CBA_Skim_Syst_TuneTest1"
-    condorOutDir1="/eos/user/i/idas/Output/cms-hcs-run2/CBA_Skim_Syst_ULv9_test11"
+    condorOutDir1="/eos/user/i/idas/Output/cms-hcs-run2/CBA_Skim_Syst_ULv9_test12"
     os.system("eos root://eosuser.cern.ch mkdir -p %s/%s"%(condorOutDir1, year))
     #condorOutDir="/cms/store/user/idas/Output/cms-hcs-run2/CBA_Skim_Syst_jet_tightID"
     #os.system("xrdfs root://se01.indiacms.res.in/ mkdir -p %s/%s"%(condorOutDir, year))
