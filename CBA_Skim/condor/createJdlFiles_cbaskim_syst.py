c990c5a2807d5624cc5e9ccb11f86ee60880c2d7
Indranil Das <indranil.das@cern.ch>
2022-01-22 09:48:16 +0530

    ready for condor job with loose PU Jet ID

--- CBA_Skim/condor/createJdlFiles_cbaskim_syst.py
+++ CBA_Skim/condor/createJdlFiles_cbaskim_syst.py
@@ -38,14 +38,14 @@ tunedict = {
     "mtopdown" : "mtopdown_TTbar"
 }
 
-if not os.path.exists("tmplog_iter1/log"):
-    os.makedirs("tmplog_iter1/log")
+if not os.path.exists("tmplog_iter2/log"):
+    os.makedirs("tmplog_iter2/log")
 condorLogDir = "log"
-tarFile = "tmplog_iter1/CBA_Skim.tar.gz"
+tarFile = "tmplog_iter2/CBA_Skim.tar.gz"
 if os.path.exists(tarFile):
 	os.system("rm %s"%tarFile)
 os.system("tar -zcvf %s ../../CBA_Skim --exclude condor"%tarFile)
-os.system("cp runCBASkim.sh tmplog_iter1/")
+os.system("cp runCBASkim.sh tmplog_iter2/")
 common_command = \
 'Universe   = vanilla\n\
 should_transfer_files = YES\n\
@@ -64,15 +64,15 @@ Log    = %s/log_$(cluster)_$(process).condor\n\n'%(condorLogDir, condorLogDir, c
 #----------------------------------------
 #Create jdl files
 #----------------------------------------
-subFile = open('tmplog_iter1/condorSubmit.sh','w')
+subFile = open('tmplog_iter2/condorSubmit.sh','w')
 for year in [2016]:
     sampleList = eval("samples_%i"%year)
     jdlName = 'submitJobs_%s.jdl'%(year)
-    jdlFile = open('tmplog_iter1/%s'%jdlName,'w')
+    jdlFile = open('tmplog_iter2/%s'%jdlName,'w')
     jdlFile.write('Executable =  runCBASkim.sh \n')
     jdlFile.write(common_command)
     #condorOutDir1="/eos/user/i/imirza/idas/Output/cms-hcs-run2/CBA_Skim_Syst_TuneTest1"
-    condorOutDir1="/eos/user/i/idas/Output/cms-hcs-run2/CBA_Skim_Syst_ULv9_DeepJet_mujets"
+    condorOutDir1="/eos/user/i/idas/Output/cms-hcs-run2/CBA_Skim_Syst_ULv9_jetpuid_lwp"
     os.system("eos root://eosuser.cern.ch mkdir -p %s/%s"%(condorOutDir1, year))
     #condorOutDir="/cms/store/user/idas/Output/cms-hcs-run2/CBA_Skim_Syst_jet_tightID"
     #os.system("xrdfs root://se01.indiacms.res.in/ mkdir -p %s/%s"%(condorOutDir, year))
