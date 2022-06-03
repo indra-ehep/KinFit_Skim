34912e11f9b6bf1f9d1e561c44a856e78996f557
Indranil Das <indranil.das@cern.ch>
2022-06-03 13:43:56 +0530

    preparing DataMCStudy directory to run in lxplus : try 8

--- DataMCStudy/run.C
+++ DataMCStudy/run.C
@@ -1,6 +1,6 @@
 //int run(const char* opt = "sample=HplusM120|year=2016|input=input/inlap/2016/HplusM120_2016.txt|index=0|run=prod|mode=proof|workers=4|syst=base|aod=nano|trs=no") // sample = data, bkg, signal
 //int run(const char* opt = "sample=MCQCDEle|year=2018|input=input/inlap/2018/MCQCDEle_2018.txt|index=0|run=prod|mode=proof|workers=1|syst=base|aod=nano|trs=no") // sample = data, bkg, signal
-int run(const char* opt = "sample=TTbar|year=2016|input=input/lnx3/2016/pre/TTbar_2016.txt|index=0|run=test|mode=proof|workers=4|syst=base|aod=nano|trs=no") // sample = data, bkg, signal
+int run(const char* opt = "sample=TTbar|year=2016|input=input/inlap/2016/pre/TTbar_2016.txt|index=0|run=test|mode=proof|workers=4|syst=base|aod=nano|trs=no") // sample = data, bkg, signal
 {
   cout << "Hello 1 " << endl;
   bool isproof  = false ;
@@ -44,18 +44,31 @@ int run(const char* opt = "sample=TTbar|year=2016|input=input/lnx3/2016/pre/TTba
   options += Form("|total=%d",idx);
   cout << "Entries " << chain->GetEntries() << endl;
   cout << "Hello 4 " << endl;
-  // if(isproof){
-  //   TProof *p = TProof::Open(Form("workers=%d",workers));
-  //   p->ClearPackages();
-  //   p->UploadPackage("loadpackages.par");
-  //   p->EnablePackage("loadpackages");
-  //   chain->SetProof();
-  // }
+
+  // TString compilerFlag = "-std=c++17 -I/Data/CMS-Software/FWLite_Test/slc7_amd64_gcc900/external/py3-correctionlib/2.0.0-0c4f44c8dd5561d8c0660135feeb81f4/lib/python3.9/site-packages/correctionlib/include -L/Data/CMS-Software/FWLite_Test/slc7_amd64_gcc900/external/py3-correctionlib/2.0.0-0c4f44c8dd5561d8c0660135feeb81f4/lib/python3.9/site-packages/correctionlib/lib -lcorrectionlib -Wl,-rpath,/Data/CMS-Software/FWLite_Test/slc7_amd64_gcc900/external/py3-correctionlib/2.0.0-0c4f44c8dd5561d8c0660135feeb81f4/lib/python3.9/site-packages/correctionlib/lib";
+
+  // // TString compilerFlag = "-std=c++17 -I/cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/py3-correctionlib/2.0.0-0c4f44c8dd5561d8c0660135feeb81f4/lib/python3.9/site-packages/correctionlib/include -L/cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/py3-correctionlib/2.0.0-0c4f44c8dd5561d8c0660135feeb81f4/lib/python3.9/site-packages/correctionlib/lib -lcorrectionlib -Wl,-rpath,/cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/py3-correctionlib/2.0.0-0c4f44c8dd5561d8c0660135feeb81f4/lib/python3.9/site-packages/correctionlib/lib";
+   
+  //  //gSystem->SetFlagsDebug(Form("%s",compilerFlag.Data()));
+  // gSystem->SetFlagsOpt(Form("%s",compilerFlag.Data()));
+
+
+  if(isproof){
+    TProof *p = TProof::Open(Form("workers=%d",workers));
+    p->ClearPackages();
+    p->UploadPackage("loadpackages.par");
+    p->EnablePackage("loadpackages");
+    chain->SetProof();
+    //p->SetFlagsOpt(Form("%s",compilerFlag.Data()));
+  }
+
   
-  // if(run == "prod")
-  //   chain->Process("SkimAna.C++", options.Data() );    
-  // else
-  //   chain->Process("SkimAna.C++", options.Data(), 1000000 );    
+
+  
+  if(run == "prod")
+    chain->Process("SkimAna.C++", options.Data() );    
+  else
+    chain->Process("SkimAna.C++", options.Data(), 1000000 );    
   
   return true;
 }
