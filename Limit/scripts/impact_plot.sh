c6ee9b2bafdc6b5f2d7942564944d3cffc560b2f
Indranil Das <indranil.das@cern.ch>
2022-07-05 11:53:26 +0530

    Updating SkimAna with (higher bin + better ctagging pt,eta) + limit code for explicit bin-by-bin fluctuations

--- Limit/scripts/impact_plot.sh
+++ Limit/scripts/impact_plot.sh
@@ -3,9 +3,13 @@ mass=$2
 #combineTool.py -M Impacts -d $t2wDataCard -m $mass --doInitialFit --robustFit 1 -t -1 --redefineSignalPOIs BR --setParameterRanges BR=0,0.1 | tee doInitialFit.log 
 #combineTool.py -M Impacts -d $t2wDataCard -m $mass --doFit --robustFit 1 -t -1 --redefineSignalPOIs BR --setParameterRanges BR=0,0.1 --parallel 3 | tee doFit.log
 
+# #AN
+# combineTool.py -M Impacts -d $t2wDataCard -m $mass --doInitialFit --robustFit 1 --redefineSignalPOIs BR --setParameterRanges BR=0,0.1 --cminDefaultMinimizerStrategy 0 | tee doInitialFit.log 
+# combineTool.py -M Impacts -d $t2wDataCard -m $mass --doFit --robustFit 1 --redefineSignalPOIs BR --setParameterRanges BR=0,0.1 --cminDefaultMinimizerStrategy 0  --parallel 3 | tee doFit.log
+
 #AN
-combineTool.py -M Impacts -d $t2wDataCard -m $mass --doInitialFit --robustFit 1 --redefineSignalPOIs BR --setParameterRanges BR=0,0.1 --cminDefaultMinimizerStrategy 0 | tee doInitialFit.log 
-combineTool.py -M Impacts -d $t2wDataCard -m $mass --doFit --robustFit 1 --redefineSignalPOIs BR --setParameterRanges BR=0,0.1 --cminDefaultMinimizerStrategy 0  --parallel 3 | tee doFit.log
+combineTool.py -M Impacts -d $t2wDataCard -m $mass --doInitialFit --robustFit 1 --setParameterRanges r=-10.,10. --cminDefaultMinimizerStrategy 0 | tee doInitialFit.log 
+combineTool.py -M Impacts -d $t2wDataCard -m $mass --doFit --robustFit 1 --setParameterRanges r=-10.,10. --cminDefaultMinimizerStrategy 0  --parallel 3 | tee doFit.log
 
 #toy
 #combineTool.py -M Impacts -d $t2wDataCard -m $mass --doInitialFit --robustFit 1 --redefineSignalPOIs BR --setParameterRanges BR=-1,1  -t -1 | tee doInitialFit.log 
