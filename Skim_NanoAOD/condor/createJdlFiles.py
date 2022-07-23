12ae4c486e0c0171ca30359ffe9c48768201536a
Indranil Das <indranil.das@cern.ch>
2022-07-23 18:27:54 +0530

    readiness for condor submission

--- Skim_NanoAOD/condor/createJdlFiles.py
+++ Skim_NanoAOD/condor/createJdlFiles.py
@@ -6,14 +6,14 @@ import sys
 sys.path.insert(0, os.getcwd().replace("condor","sample"))
 from NanoAOD_Gen_SplitJobs_cff import Samples_2016, Samples_2017, Samples_2018 
 
-if not os.path.exists("tmpSub_2018_iter2/log"):
-    os.makedirs("tmpSub_2018_iter2/log")
+if not os.path.exists("tmpSub_2016-8_recover/log"):
+    os.makedirs("tmpSub_2016-8_recover/log")
 condorLogDir = "log"
-tarFile = "tmpSub_2018_iter2/Skim_NanoAOD.tar.gz"
+tarFile = "tmpSub_2016-8_recover/Skim_NanoAOD.tar.gz"
 if os.path.exists(tarFile):
 	os.system("rm %s"%tarFile)
 os.system("tar -zcvf %s ../../Skim_NanoAOD --exclude condor"%tarFile)
-os.system("cp runMakeSkims.sh tmpSub_2018_iter2/")
+os.system("cp runMakeSkims.sh tmpSub_2016-8_recover/")
 common_command = \
 'Universe   = vanilla\n\
 should_transfer_files = YES\n\
@@ -23,7 +23,8 @@ use_x509userproxy = true\n\
 RequestDisk = 30GB\n\
 +BenchmarkJob = True\n\
 #+JobFlavour = "workday"\n\
-+MaxRuntime = 39600\n\
+#+MaxRuntime = 39600\n\
++MaxRuntime = 7200\n\
 Output = %s/log_$(cluster)_$(process).stdout\n\
 Error  = %s/log_$(cluster)_$(process).stderr\n\
 Log    = %s/log_$(cluster)_$(process).condor\n\n'%(condorLogDir, condorLogDir, condorLogDir)
@@ -31,16 +32,17 @@ Log    = %s/log_$(cluster)_$(process).condor\n\n'%(condorLogDir, condorLogDir, c
 #----------------------------------------
 #Create jdl files
 #----------------------------------------
-subFile = open('tmpSub_2018_iter2/condorSubmit.sh','w')
-#for year in [2016,2017,2018]:
-for year in [2018]:
+subFile = open('tmpSub_2016-8_recover/condorSubmit.sh','w')
+for year in [2016,2017,2018]:
+#for year in [2018]:
     sampleList = eval("Samples_%i"%year)
     jdlName = 'submitJobs_%s.jdl'%(year)
-    jdlFile = open('tmpSub_2018_iter2/%s'%jdlName,'w')
+    jdlFile = open('tmpSub_2016-8_recover/%s'%jdlName,'w')
     jdlFile.write('Executable =  runMakeSkims.sh \n')
     jdlFile.write(common_command)
     #condorOutDir="/eos/user/i/idas/Output/cms-hcs-run2/Skim_NanoAODUL"
-    condorOutDir="/eos/cms/store/group/phys_higgs/HiggsExo/idas/cms-hcs-run2/Skim_NanoAODUL"
+    #condorOutDir="/eos/cms/store/group/phys_higgs/HiggsExo/idas/cms-hcs-run2/Skim_NanoAODUL"
+    condorOutDir="/eos/user/d/dugad/idas/Output/cms-hcs-run2/Skim_NanoAODUL"
     os.system("eos root://eosuser.cern.ch mkdir -p %s/%s"%(condorOutDir, year))
     jdlFile.write("X=$(step)+1\n")
     
