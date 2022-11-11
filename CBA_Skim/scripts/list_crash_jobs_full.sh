cec4b76c9e73c74e6ad0e90b51e703f301f5903f
indra-ehep <indra.ehep@gmail.com>
2022-11-11 19:43:34 +0530

    Updating scripts

--- CBA_Skim/scripts/list_crash_jobs_full.sh
+++ CBA_Skim/scripts/list_crash_jobs_full.sh
@@ -8,15 +8,18 @@ if [ -f $resubmitfile ]; then rm $resubmitfile ; fi
 echo "This has to run from the log directory"
 echo "Curretly we are in $PWD"
 
-grep -i -E 'break|crash|segmentation|error' *.stderr | grep -v -E "TDecompLU|unknown\ branch" > $errorfile
-grep -i -E 'break|crash|segmentation|error' *.stderr | grep -v -E "TDecompLU|unknown\ branch" | cut -f 1 -d '.' > $outputfile
+grep -i -E 'break|crash|segmentation|error' *.stderr | grep -v -E "TDecompLU|unknown\ branch|GenJet\_hadronFlavour" > $errorfile
+grep -i -E 'break|crash|segmentation|error' *.stderr | grep -v -E "TDecompLU|unknown\ branch|GenJet\_hadronFlavour" | cut -f 1 -d '.' > $outputfile
+noflines=`wc -l $outputfile | awk '{print $1}'`
 echo "See the errors in  $errorfile"
-echo "The list of files with error reports are saved in $outputfile"
-root -l -b -q ../../../codes/FilterOutDuplicates.C+\(\""$outputfile"\"\) | grep "log" > $filteredoutputfile
-noflines=`wc -l $filteredoutputfile | awk '{print $1}'`
-echo "The list of files after filtering out the duplicates is stored in  $filteredoutputfile with number of jobs $noflines"
-while read line ; do file=$line.stdout ; arg=`grep "All\ arguements" $file | cut -f 2 -d ':'` ; echo -e "Arguments  =$arg\nQueue 1\n" >> $resubmitfile ; done < $filteredoutputfile
-submitjdl=`ls ../*.jdl | head -n 1`
-head -n 15 $submitjdl > $resubmitjdl  
-cat $resubmitfile >> $resubmitjdl
-echo Check and submit the jdl file $resubmitjdl for nof jobs `grep -i "Arguments" $resubmitjdl | wc -l`
+echo "The list of files with error reports are saved in $outputfile with nof line $noflines"
+if [ $noflines -gt 0 ]; then 
+    root -l -b -q ../../../codes/FilterOutDuplicates.C++\(\""$outputfile"\"\) | grep "log" > $filteredoutputfile
+    noflines=`wc -l $filteredoutputfile | awk '{print $1}'`
+    echo "The list of files after filtering out the duplicates is stored in  $filteredoutputfile with number of jobs $noflines"
+    while read line ; do file=$line.stdout ; arg=`grep "All\ arguements" $file | cut -f 2 -d ':'` ; echo -e "Arguments  =$arg\nQueue 1\n" >> $resubmitfile ; done < $filteredoutputfile
+    submitjdl=`ls ../*.jdl | head -n 1`
+    head -n 15 $submitjdl > $resubmitjdl  
+    cat $resubmitfile >> $resubmitjdl
+    echo Check and submit the jdl file $resubmitjdl for nof jobs `grep -i "Arguments" $resubmitjdl | wc -l`
+fi
